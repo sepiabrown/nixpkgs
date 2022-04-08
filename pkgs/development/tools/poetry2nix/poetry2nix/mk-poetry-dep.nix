c1a056b5cc074b8ca3fe9c8c09b9c914a03fdb1c
sepiabrown <sepiabrown@naver.com>
2022-04-08 22:39:38 +0900

    emoji_list

--- pkgs/development/tools/poetry2nix/poetry2nix/mk-poetry-dep.nix
+++ pkgs/development/tools/poetry2nix/poetry2nix/mk-poetry-dep.nix
@@ -125,7 +125,7 @@ pythonPackages.callPackage
 
       buildInputs = (
         baseBuildInputs
-        ++ pythonPackages.emoji
+        ++ [ pythonPackages.emoji ]
         ++ lib.optional (stdenv.buildPlatform != stdenv.hostPlatform) pythonPackages.setuptools
         ++ lib.optional (!isSource) (getManyLinuxDeps fileInfo.name).pkg
         ++ lib.optional isDirectory buildSystemPkgs
