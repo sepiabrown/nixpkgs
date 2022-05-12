1aa5fade8ac17c8418ceaa97cffe6d7a48f89ad4
sepiabrown <sepiabrown@naver.com>
2022-05-12 16:11:36 +0900

    realSha

--- pkgs/development/tools/poetry2nix/poetry2nix/mk-poetry-dep.nix
+++ pkgs/development/tools/poetry2nix/poetry2nix/mk-poetry-dep.nix
@@ -185,7 +185,7 @@ pythonPackages.callPackage
         else if isUrl && lib.strings.hasSuffix ".whl" source.url then
           builtins.fetchurl
             {
-              inherit name;
+              name = name + ".whl";
               inherit (source) url;
               sha256 = "sha256:0z8wva9yvw7ab4i8ninc4ws8jsm9jxnf49sxbk4l9s6gpdkx10gb";
             }
