68c80b4f79b44b58bf167327a460e9c29eeecfca
sepiabrown <sepiabrown@naver.com>
2022-05-14 20:08:18 +0900

    hatchling

--- pkgs/development/tools/poetry2nix/poetry2nix/overrides/default.nix
+++ pkgs/development/tools/poetry2nix/poetry2nix/overrides/default.nix
@@ -571,7 +571,7 @@ lib.composeManyExtensions [
 
       hatchling = super.hatchling.overridePythonAttrs (old: {
         propagatedBuildInputs = [
-          packaging
+          self.packaging
         ];
       });
 
