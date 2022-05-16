99345b5aed53a37a9c6519de49ef2e4ee0c3b649
sepiabrown <sepiabrown@naver.com>
2022-05-17 07:07:48 +0900

    parse_url

--- pkgs/development/tools/poetry2nix/poetry2nix/overrides/default.nix
+++ pkgs/development/tools/poetry2nix/poetry2nix/overrides/default.nix
@@ -2161,7 +2161,7 @@ lib.composeManyExtensions [
             '';
             nativeBuildInputs =
               (old.nativeBuildInputs or [ ])
-              ++ [ pkgs.autoPatchelfHook stdenv.cc.cc stdenv.cc.cc.lib pkgs.glibc pkgs.gcc-unwrapped ]
+              ++ [ pkgs.autoPatchelfHook ]
               ++ lib.optionals enableCuda [ cudatoolkit_joined pkgs.addOpenGLRunpath ];
             buildInputs =
               (old.buildInputs or [ ])
@@ -2175,10 +2175,7 @@ lib.composeManyExtensions [
                 cuda_nvcc
                 pkgs.magma
                 nccl
-                stdenv.cc.cc
-                stdenv.cc.cc.lib
                 pkgs.glibc
-                pkgs.gcc-unwrapped
               ];
             propagatedBuildInputs = [
               self.numpy
