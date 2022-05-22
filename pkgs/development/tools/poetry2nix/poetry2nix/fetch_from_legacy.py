73aa2442dc0e91fb2a28279b7e74bc6a380190ab
sepiabrown <sepiabrown@naver.com>
2022-05-23 05:15:48 +0900

    url

--- pkgs/development/tools/poetry2nix/poetry2nix/fetch_from_legacy.py
+++ pkgs/development/tools/poetry2nix/poetry2nix/fetch_from_legacy.py
@@ -67,7 +67,7 @@ index_url2 = urlunparse(
     (
         url2.scheme,
         url2.netloc,
-        url2.patl + url2.path.replace("-","_") + ".html",
+        url2.patl + str(url2.path).replace("-","_") + ".html",
         None,
         None,
         None,
